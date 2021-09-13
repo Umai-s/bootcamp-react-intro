5fe50b6502281cd6d409f1189483ad249dd7777c
Umaima <umaima.salem@gmail.com>
2021-09-14 02:32:29 +0500

    Added Student component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Student from './Student';
 
 function App() {
   return (
     <div className="App">
       Hello world! Starting React..
+      <Student name={Umaima}/>
+      <hr/>
+      <Student name={Hafsah}/>
+      <hr/>
+      <Student name={Ayesha}/>
     </div>
   );
 }
---
+++ src/Student.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Student(props) {
+  return (
+    <div>
+      <h1>Student : {props.name}</h1>
+    </div>
+  );
+}
+
+export default Student;
